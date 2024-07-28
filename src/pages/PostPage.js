1e7e48d855f07d71025e7a908b2cf2991e2f303c
Your Name <you@example.com>
2024-07-28 23:06:03 +0530

    commiting the changes

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { UserContext } from "../UserContext"
+import { formatISO9075 } from "date-fns"
 export default function PostPage(cover) {
     
     const [postInfo,setPostInfo] = useState(null)
@@ -25,12 +26,23 @@ export default function PostPage(cover) {
 
     return(
         <div>
-            <div className="image">
-                <img src={`http://localhost:4000/${postInfo.cover}`} alt="" />
-            </div>
+            
             <h1>
                 {postInfo.title}
             </h1>
+            
+            <div className="image">
+                <img src={`http://localhost:4000/${postInfo.cover}`} alt="" />
+            </div>
+            <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+            <div className="author">by @{postInfo.author.username}</div>
+            {userInfo.id === postInfo.author._id && (
+                <div> 
+                    <Link className="edit-btn" to={`/edit/${postInfo._id}`}>
+                            edit this page
+                    </Link>
+                </div>
+            )}
             <div dangerouslySetInnerHTML={{__html:postInfo.content}} />
         </div>
  
